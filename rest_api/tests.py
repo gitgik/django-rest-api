25943396d5e6292032592b2a8b363efae7515640
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-04 16:04:04 +0300

    (chore) write tests for models

--- rest_api/tests.py
+++ rest_api/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the bucketlist model."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.name = "Write world class code"
+        self.bucketlist = Bucketlist(name=self.name)
+
+    def test_model_can_create_a_bucketlist(self):
+        """Test the bucketlist model can create a bucketlist."""
+        old_count = Bucketlist.objects.count()
+        self.bucketlist.save()
+        new_count = Bucketlist.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+    def test_model_returns_readable_representation(self):
+        """Test a readable string is returned for the model instance."""
+        self.assertEqual(str(self.bucketlist), self.name)
