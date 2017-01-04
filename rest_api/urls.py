c32048190731c26e787cb8d54f32fe700b8f080f
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-04 23:50:48 +0300

    (chore) define urls for querying users

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView
+from .views import CreateView, DetailsView, UserView, UserDetailsView
 
 
 urlpatterns = {
@@ -9,6 +9,9 @@ urlpatterns = {
         DetailsView.as_view(), name="details"),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'^users/$', UserView.as_view(), name="users"),
+    url(r'users/(?P<pk>[0-9]+)/$',
+        UserDetailsView.as_view(), name="user_details")
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
