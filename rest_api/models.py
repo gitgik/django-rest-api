1c60e523983c897eb9f615c29f7306a84807c6fa
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-04 15:41:52 +0300

    (feature) create the bucketlist model

--- rest_api/models.py
+++ rest_api/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Bucketlist(DateMixin):
+    """This class represents the bucketlist model."""
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
