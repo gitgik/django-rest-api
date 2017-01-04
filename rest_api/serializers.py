417996c966a5faafb18f952d640ddcb7816c755d
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-04 16:19:30 +0300

    (feature) define serializer to map with the model

---
+++ rest_api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Bucketlist
+
+
+class BucketlistSerializers(serializers.ModelSerializer):
+    """Serializer to map the model instance into json format."""
+
+    class Meta:
+        """Map this serializer to a model and their fields."""
+        model = Bucketlist
+        fields = ('name', 'date_created', 'date_modified')
+        read_only_fields = ('date_created' 'date_modified')
