622545bb8ada1074088b6f12687c17d4d6b9aa16
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-05 17:30:12 +0300

    (feature) test the api can create a bucketlist

--- rest_api/tests.py
+++ rest_api/tests.py
@@ -21,3 +21,18 @@ class ModelTestCase(TestCase):
     def test_model_returns_readable_representation(self):
         """Test a readable string is returned for the model instance."""
         self.assertEqual(str(self.bucketlist), self.name)
+
+
+class ViewTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Go to Ibiza'}
+        self.bucketlist = Bucketlist(name=self.name)
+
+    def test_api_can_create_a_bucketlist(self):
+        """Test the api has bucket creation capability."""
+        response = self.client.post(reverse('create'), self.bucketlist_data)
+        self.assertEqual(response.status_code, 201)
