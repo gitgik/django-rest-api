73fe18da19bf438f82efa7685e8ca788f4543a24
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-06 18:50:43 +0300

    (chore) add test for deleting a bucketlist

--- rest_api/tests.py
+++ rest_api/tests.py
@@ -45,16 +45,26 @@ class ViewTestCase(TestCase):
         bucketlist = Bucketlist.objects.get()
         response = self.client.get(
             reverse('details'),
-            kwargs={'pk': bucketlist.id}, format=="json")
+            kwargs={'pk': bucketlist.id}, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertContains(response, bucketlist)
 
     def test_api_can_update_bucketlist(self):
         """Test the api can update a given bucketlist."""
-        change_bucketlist ={'name': 'Something new'}
+        change_bucketlist = {'name': 'Something new'}
         res = self.client.put(
-            reverse('api.bucketlist', kwargs={'pk': bucketlist.id}),
+            reverse('details', kwargs={'pk': bucketlist.id}),
             change_bucketlist, format='json'
         )
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_bucketlist(self):
+        """Test the api can delete a bucketlist."""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
