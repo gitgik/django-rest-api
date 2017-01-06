1e15d4b63bb811bf30a087a67db7d9cf3ec265d0
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-06 22:37:43 +0300

    (chore) add url for obtaining auth token

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView, DetailsView, UserView, UserDetailsView
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = {
     url(r'^bucketlists/$', CreateView.as_view(), name="create"),
@@ -11,7 +11,8 @@ urlpatterns = {
                                namespace='rest_framework')),
     url(r'^users/$', UserView.as_view(), name="users"),
     url(r'users/(?P<pk>[0-9]+)/$',
-        UserDetailsView.as_view(), name="user_details")
+        UserDetailsView.as_view(), name="user_details"),
+    url(r'^api-token-auth/', obtain_auth_token),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
