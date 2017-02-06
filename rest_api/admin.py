6c03b52b7e3b24ca71914d2fb95d9bfcf085a000
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-06 21:17:15 +0300

    (chore) update migrations, monkey patch the token admin for future customizations

--- rest_api/admin.py
+++ rest_api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Bucketlist
 
+from rest_framework.authtoken.admin import TokenAdmin
+
+TokenAdmin.raw_id_fields = ('user',)
+
 # Register your models here.
 admin.site.register(Bucketlist)
