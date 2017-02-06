4b6f67db143976932dfaa949261c5b2aa5791643
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-06 23:17:01 +0300

    (chore) document receiver

--- rest_api/models.py
+++ rest_api/models.py
@@ -21,7 +21,7 @@ class Bucketlist(models.Model):
         """Return a human readable representation of the model instance."""
         return "{}".format(self.name)
 
-
+# This receiver handles token creation when a new user is created.
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
