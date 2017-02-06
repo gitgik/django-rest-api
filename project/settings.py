ee74ca596162a9902e02e0f5959fd4c1dcec0871
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-06 23:30:35 +0300

    edit settings

--- project/settings.py
+++ project/settings.py
@@ -29,11 +29,9 @@ ALLOWED_HOSTS = ['localhost', 'testserver']
 
 
 REST_FRAMEWORK = {
-    
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     )
