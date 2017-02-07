9421fcae9431790a05cf76d44977bfb5ac277748
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-07 22:21:11 +0300

    (chore) permit session and basic authentication in settings

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     )
 }
 
