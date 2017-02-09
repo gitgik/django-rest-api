c2e9611f03c3b05f04dd397dfadb1ee0b2bbd987
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-09 15:05:12 +0300

    (chore) remove session  authentication

--- project/settings.py
+++ project/settings.py
@@ -35,7 +35,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
     )
 }
 
